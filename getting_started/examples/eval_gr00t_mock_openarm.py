abc8f97dd58a2f1b3fc890150f0bff2f85de7bd7
Thomason Zhou <t95zhou@uwaterloo.ca>
2025-04-04 22:57:21 +0900

    Finish mock data

--- getting_started/examples/eval_gr00t_mock_openarm.py
+++ getting_started/examples/eval_gr00t_mock_openarm.py
@@ -5,11 +5,12 @@ from typing import Dict, Any
 import numpy as np
 
 raw_obs_dict: Dict[str, Any] = {
-    "video.ego_view": np.zeros((480, 640, 3), dtype=np.uint8),
+    "video.ego_view": np.zeros((1, 480, 640, 3), dtype=np.uint8),
     "state.single_arm": np.zeros((1, 7)),
     "state.gripper": np.zeros((1, 1)),
-    "annotation.human.action.task_description": ["pick up the bottle from the counter and place it inside the bin."],
+    "annotation.human.action.task_description": "pick up the bottle from the counter and place it inside the bin.",
 } 
 
 policy = ExternalRobotInferenceClient(host="localhost", port=8888)
 raw_action_chunk: Dict[str, Any] = policy.get_action(raw_obs_dict)
+print("Raw action chunk:", raw_action_chunk)
